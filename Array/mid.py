3a3f63ca6bc15a41e379cbb8c40949c075ffcb87
yashar-sb-sb <yashar_sb_sb@yahoo.com>
2016-02-22 23:18:54 +0330

    Create mid.py

---
+++ Array/mid.py
@@ -0,0 +1,20 @@
+randomizedMid(a, **args)
+'''
+a has to be a list
+args could have l or r or both
+if non of l nor r is presented then the function will perform the operation on the whole of a
+if only l is presented the operation will be performed on range [l:]
+if only r is presented the operation will be performed on range [:r]
+if both are presented the operation will be performed on range [l:r]
+'''
+
+
+deterministicMid(a, **args)
+'''
+a has to be a list
+args could have l or r or both
+if non of l nor r is presented then the function will perform the operation on the whole of a
+if only l is presented the operation will be performed on range [l:]
+if only r is presented the operation will be performed on range [:r]
+if both are presented the operation will be performed on range [l:r]
+'''
